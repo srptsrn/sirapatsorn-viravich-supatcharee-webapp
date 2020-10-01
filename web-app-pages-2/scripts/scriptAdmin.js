2c841c48f11d08db5b68b7bcbe996e41b7ce2eda
Viravich <Viravich.phi@kkumail.com>
2020-10-01 20:14:08 +0700

    viravich prototype contact page(fixed)

---
+++ web-app-pages-2/scripts/scriptAdmin.js
@@ -0,0 +1,6 @@
+const collectionImg = {
+    visibility: {
+        show: "https://www.flaticon.com/svg/static/icons/svg/565/565654.svg",
+        hide: "https://www.flaticon.com/svg/static/icons/svg/565/565655.svg",
+    },
+};
