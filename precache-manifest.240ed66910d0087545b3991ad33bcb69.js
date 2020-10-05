6b489495071622806ae2f80b8433534fe43f44e9
supatchareec <supatcharee_chaiya@kkumail.com>
2020-10-05 15:01:55 +0700

    Updates

---
+++ precache-manifest.240ed66910d0087545b3991ad33bcb69.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "bee3a06b3b7dca472ded5fefa8bfd601",
+    "url": "/%7BGithub-repo-name%7D/index.html"
+  },
+  {
+    "revision": "9001aafaa7e276b9727a",
+    "url": "/%7BGithub-repo-name%7D/static/css/main.523f09c5.chunk.css"
+  },
+  {
+    "revision": "1f27ee7a2dc2381fe0da",
+    "url": "/%7BGithub-repo-name%7D/static/js/2.dac390d1.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/%7BGithub-repo-name%7D/static/js/2.dac390d1.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "9001aafaa7e276b9727a",
+    "url": "/%7BGithub-repo-name%7D/static/js/main.c10e9ca9.chunk.js"
+  },
+  {
+    "revision": "2a2449d17832aa7768e2",
+    "url": "/%7BGithub-repo-name%7D/static/js/runtime-main.f7af3e77.js"
+  },
+  {
+    "revision": "ee7cd8ed2dcec943251eb2763684fc6f",
+    "url": "/%7BGithub-repo-name%7D/static/media/logo.ee7cd8ed.svg"
+  }
+]);
