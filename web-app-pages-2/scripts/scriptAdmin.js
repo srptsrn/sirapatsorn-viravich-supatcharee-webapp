67f6b3b902d9b0b0630088a3135c103813c42a68
Viravich <Viravich.phi@kkumail.com>
2020-10-05 11:13:29 +0700

    viravich prototype add responsive

--- web-app-pages-2/scripts/scriptAdmin.js
+++
@@ -1,6 +0,0 @@
-const collectionImg = {
-    visibility: {
-        show: "https://www.flaticon.com/svg/static/icons/svg/565/565654.svg",
-        hide: "https://www.flaticon.com/svg/static/icons/svg/565/565655.svg",
-    },
-};
