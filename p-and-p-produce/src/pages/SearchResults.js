16088b1e8397cb812937471380283321db45071c
supatchareec <supatcharee_chaiya@kkumail.com>
2020-12-05 23:30:31 +0700

    edit bug auto slide banner

--- p-and-p-produce/src/pages/SearchResults.js
+++ p-and-p-produce/src/pages/SearchResults.js
@@ -7,26 +7,24 @@ import { BrowserRouter as Router, Switch, Route, Link } from "react-router-dom";
 import firebase from "../components/Firebase.js";
 
 const SearchResults = () => {
-
   const connector = firebase.firestore();
 
-  const [search, setSearch] = useState('');
+  const [search, setSearch] = useState("");
 
   const [products, setProducts] = useState([]);
 
   const [searchResults, setSearchResults] = useState([]);
 
   const filter = (search) => {
-    if (search != '') {
-      console.log(products)
-      const result = products.filter(product => String(product.name.toLowerCase()).includes(search.toLowerCase()));
-      console.log(result)
-      setSearchResults(result)
+    if (search != "") {
+      console.log(products);
+      const result = products.filter((product) =>
+        String(product.name.toLowerCase()).includes(search.toLowerCase())
+      );
+      console.log(result);
+      setSearchResults(result);
     }
-
-  }
-
-  
+  };
 
   useEffect(() => {
     const getData = async () => {
@@ -44,28 +42,26 @@ const SearchResults = () => {
             if (JSON.stringify(array[0]) === JSON.stringify({ img: "" })) {
               array.shift();
             }
-            
           });
-          setProducts(array); 
+          setProducts(array);
           setSearchResults(array);
         });
-    }
-    setSearch(new URLSearchParams(window.location.search).get('search'));
+    };
+    setSearch(new URLSearchParams(window.location.search).get("search"));
     getData();
-  }, [])
+  }, []);
 
   useEffect(() => {
-    console.log(search)
+    console.log(search);
     filter(search);
-    if (search === '') {
-      setSearchResults(products)
+    if (search === "") {
+      setSearchResults(products);
     }
-    
-  }, [search, products])
+  }, [search, products]);
 
   const handleSearchValueChange = (event) => {
     setSearch(event.target.value);
-  }
+  };
 
   return (
     <div className="app-search-results">
@@ -77,44 +73,49 @@ const SearchResults = () => {
       </div>
       <div class="search-tab-search-results">
         <div class="icon-search"></div>
-        <input id="search_web"
-          type="text" class="search_web"
+        <input
+          id="search_web"
+          type="text"
+          class="search_web"
           value={search}
           onChange={handleSearchValueChange}
-          placeholder="Search" />
+          placeholder="Search"
+        />
       </div>
       <div className="row-search-results">
         {searchResults.map((value, index) => {
-          return <div className="column-search-results">
-            <Link to="/product-details" className="link-no-underline">
-              <div className="card-search-results">
-                <img src={Picture}></img>
-                <p className="product-name">{value.name}</p>
-                <p className="product-price">฿{value.price}</p>
-                <input
-                  type="number"
-                  className="input-product-quantity"
-                  defaultValue="1"
-                  min="1"
-                ></input>
-                <Link to="/cart">
-                  <button type="button" className="btn-add-to-cart">
-                    Add to Cart{" "}
-                  </button>
-                </Link>
-                <Link to="/cart">
-                  <button type="button" className="btn-buy-now">
-                    Buy now
-              </button>
-                </Link>
-              </div>
-            </Link>
-          </div>
+          return (
+            <div className="column-search-results">
+              <Link to="/product-details" className="link-no-underline">
+                <div className="card-search-results">
+                  <img src={Picture}></img>
+                  <p className="product-name">{value.name}</p>
+                  <p className="product-price">฿{value.price}</p>
+                  <input
+                    type="number"
+                    className="input-product-quantity"
+                    defaultValue="1"
+                    min="1"
+                  ></input>
+                  <Link to="/cart">
+                    <button type="button" className="btn-add-to-cart">
+                      Add to Cart{" "}
+                    </button>
+                  </Link>
+                  <Link to="/cart">
+                    <button type="button" className="btn-buy-now">
+                      Buy now
+                    </button>
+                  </Link>
+                </div>
+              </Link>
+            </div>
+          );
         })}
       </div>
       <Footer></Footer>
     </div>
   );
-}
+};
 
 export default SearchResults;
