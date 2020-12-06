c962f1c9200fdfbe1af98e2e28744fc67bc52463
Viravich <Viravich.phi@kkumail.com>
2020-12-07 01:01:22 +0700

    fix path css and script on paymentPage.html

--- p-and-p-produce/src/pages/BestSellers.js
+++ p-and-p-produce/src/pages/BestSellers.js
@@ -8,18 +8,21 @@ import firebase from "../components/Firebase.js";
 import CatagoryProduct from "../components/CatagoryProduct.js";
 
 const BestSellers = () => {
-  return (
-    <div className="app-best-sellers">
-      <NavBar bestSellers></NavBar>
-      <div className="head-best-sellers">
-        <div>
-          <h2 className="best-sellers-center"> Best Sellers </h2>
-        </div>
-      </div>
-      <CatagoryProduct bestSellers></CatagoryProduct>
-      <Footer></Footer>
-    </div>
-  );
+    return ( <
+        div className = "app-best-sellers" >
+        <
+        NavBar bestSellers > < /NavBar> <
+        div className = "head-best-sellers" >
+        <
+        div >
+        <
+        h2 className = "best-sellers-center" > Best Sellers < /h2> <
+        /div> <
+        /div> <
+        CatagoryProduct bestSellers > < /CatagoryProduct> <
+        Footer > < /Footer> <
+        /div>
+    );
 }
 
 export default BestSellers;
--- web-app-pages-2/scripts/scriptPayment.js
+++ web-app-pages-2/scripts/scriptPayment.js
@@ -3,17 +3,7 @@ function buttonConfirm() {
     var userReplyConfirm;
     var userReplyCancel;
     var comfirm;
-    // alert(comfirm);
 
-    // // if (userReply == true) {
-    // //     alert("ยืนยันข้อมูล")
-    // // }
-    // if (comfirm("กรุณาตรวจสอบข้อมูลให้ถูกต้อง หากกดยืนยันแล้วจะไม่สามารถเปลี่ยนแปลงได้") == true) {
-    //     userReply = "ยืนยัน"
-    // } else {
-    //     userReply = "ยกเลิก"
-    // }
-    // alert(userReply);
 
     if (confirm("กรุณาตรวจสอบข้อมูลให้ถูกต้อง หากกดยืนยันแล้วจะไม่สามารถเปลี่ยนแปลงได้")) {
         alert(userReplyConfirm = "ยืนยัน");
